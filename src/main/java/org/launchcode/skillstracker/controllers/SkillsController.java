690cbe6afc04505fa20e5c3abed731030f6544dc
monivg <monicav2013@gmail.com>
2023-04-24 18:48:28 -0500

    Initial commit

---
+++ src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
@@ -0,0 +1,77 @@
+package org.launchcode.skillstracker.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class SkillsController {
+    @GetMapping("/")
+    public String displayHomePage() {
+        return "<html>" +
+                "<body>" +
+                "<h1>Skills Tracker</h1>" +
+                "<h3>We have a few skills we would like to learn. Here is the list!</h3>" +
+                "<ol>" +
+                "<li>Java</li>" +
+                "<li>JavaScript</li>" +
+                "<li>TypeScript</li>" +
+                "<li>Python</li>" +
+                "<li>Ruby</li>" +
+                "</ol>" +
+                "<p>Click <a href='/form'>here</a> to select your favorite.</p>" +
+                "</body>" +
+                "</html>";
+    }
+
+    @GetMapping("/form")
+    public String displaySkillsForm() {
+        return "<html>" +
+                "<body>" +
+                "<form action='/form' method='POST'>" +
+                "<label>Name:</label>" +
+                "<input type='text' name='userName' /><br />" +
+                "<label>My favorite language:</label>" +
+                "<select name='option1'>" +
+                "<option value='Java'>Java</option>" +
+                "<option value='JavaScript'>JavaScript</option>" +
+                "<option value='TypeScript'>TypeScript</option>" +
+                "<option value='Python'>Python</option>" +
+                "<option value='Ruby'>Ruby</option>" +
+                "</select><br />" +
+                "<label>My second favorite language:</label>" +
+                "<select name='option2'>" +
+                "<option value='Java'>Java</option>" +
+                "<option value='JavaScript'>JavaScript</option>" +
+                "<option value='TypeScript'>TypeScript</option>" +
+                "<option value='Python'>Python</option>" +
+                "<option value='Ruby'>Ruby</option>" +
+                "</select><br />" +
+                "<label>My third favorite language:</label>" +
+                "<select name='option3'>" +
+                "<option value='Java'>Java</option>" +
+                "<option value='JavaScript'>JavaScript</option>" +
+                "<option value='TypeScript'>TypeScript</option>" +
+                "<option value='Python'>Python</option>" +
+                "<option value='Ruby'>Ruby</option>" +
+                "</select><br />" +
+                "<input type='submit' value='Submit' />" +
+                "</form>" +
+                "</body>" +
+                "</html>";
+    }
+
+    @PostMapping("/form")
+    public String skillsOutput(@RequestParam String userName, String option1, String option2, String option3) {
+        return "<html>" +
+                "<body>" +
+                "<h1>" + userName + "</h1>" +
+                "<ol>" +
+                "<li>" + option1 + "</li>" +
+                "<li>" + option2 + "</li>" +
+                "<li>" + option3 + "</li>" +
+                "</ol>" +
+                "</body>" +
+                "</html>";
+    }
+
+}
